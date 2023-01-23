d616833e62aa70337da7debba4716416254ba87f
biancabera <biancabera@gmail.com>
2023-01-23 20:30:03 -0300

    Requisito 3

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Login() {
   const [error] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useState(true);
+
+  useEffect(() => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = email.match(regex);
+    const passwordLength = password.length >= Number('6');
+    if (emailRegex && passwordLength) setLogin(false);
+    if (!emailRegex && passwordLength) setLogin(true);
+  }, [email, password]);
+
   return (
     <div>
       <Redirect to="/login" />
@@ -11,15 +23,18 @@ function Login() {
           type="email"
           placeholder="Insira seu e-mail"
           data-testid="common_login__input-email"
+          onChange={ (event) => setEmail(event.target.value) }
         />
         <input
           type="password"
           placeholder="Insira sua senha"
           data-testid="common_login__input-password"
+          onChange={ (event) => setPassword(event.target.value) }
         />
         <button
           type="button"
           data-testid="common_login__button-login"
+          disabled={ login }
         >
           Entrar
         </button>
@@ -29,7 +44,7 @@ function Login() {
         >
           Cadastre-se
         </button>
-        {error && (
+        { error && (
           <span
             data-testid="common_login__element-invalid-email"
           >
