34784b80158b0182d305d633edd5fedeb30335ef
adevmir <ademirvalonqar@gmail.com>
2023-01-24 18:03:18 -0300

    Requisito 07 - Implementa os elementos da tela de registro

--- front-end/src/Pages/Register.js
+++ front-end/src/Pages/Register.js
@@ -2,9 +2,41 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Register() {
+  const [error] = useState(false);
   return (
     <div>
       <Redirect to="/register" />
+      <h1> Cadastro </h1>
+      <form>
+        <input
+          type="name"
+          placeholder="Insira seu nome"
+          data-testid="common_register__input-name"
+        />
+        <input
+          type="email"
+          placeholder="Insira seu e-mail"
+          data-testid="common_register__input-email"
+        />
+        <input
+          type="password"
+          placeholder="Insira sua senha"
+          data-testid="common_register__input-password"
+        />
+        <button
+          type="button"
+          data-testid="common_register__button-register"
+        >
+          Cadastrar
+        </button>
+      </form>
+      {error && (
+        <span
+          data-testid="common_register__element-invalid_register"
+        >
+          E-mail/senha invalidos.
+        </span>
+      )}
     </div>
   );
 }
