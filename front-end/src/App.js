6d4f17f7ed8f02cc599adaf23cf15ea41798f5b9
Anderson Correa <andersonfpcorrea@gmail.com>
2023-01-24 17:26:31 -0300

    refactor: remove page-not-found component and route

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
 import Checkout from './Pages/Checkout';
-import PageNotFound from './Pages/PageNotFound';
 import OrderDetails from './Pages/OrderDetails';
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
       <Route exact path="/login" component={ Login } />
       <Route exact path="/customer/checkout" component={ Checkout } />
       <Route exact path="/customer/orders/:id" component={ OrderDetails } />
-      <Route exatc path="/" component={ Login } />
-      <Route path="*" component={ PageNotFound } />
+      <Route path="/" component={ Login } />
     </Switch>
   );
 }
--- front-end/src/Pages/PageNotFound.js
+++
@@ -1,7 +0,0 @@
-export default function PageNotFound() {
-  return (
-    <div style={ { display: 'grid', placeItems: 'center' } }>
-      <h1>🤯 404 - Page not found</h1>
-    </div>
-  );
-}
