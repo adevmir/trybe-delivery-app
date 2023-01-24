a9f9ba0cdb13dd31d8dea570de2d68c0093f499c
adevmir <ademirvalonqar@gmail.com>
2023-01-24 19:56:11 -0300

    Requisito 08 - Implementa na tela de registro a verificacao dos dados do formulario de forma que se invalidos desabilita o botao de registro

--- front-end/src/Pages/Register.js
+++ front-end/src/Pages/Register.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 function Register() {
-  const [error] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [register, setRegister] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const nameLength = name.length >= Number('12');
+    const emailRegex = email.match(regex);
+    const passwordLength = password.length >= Number('6');
+    if (nameLength && emailRegex && passwordLength) {
+      setRegister(false);
+      setError(false);
+    }
+    if (!nameLength || !emailRegex || !passwordLength) {
+      setRegister(true);
+      setError(true);
+    }
+  }, [name, email, password]);
+
   return (
     <div>
       <Redirect to="/register" />
@@ -12,20 +32,24 @@ function Register() {
           type="name"
           placeholder="Insira seu nome"
           data-testid="common_register__input-name"
+          onChange={ (event) => setName(event.target.value) }
         />
         <input
           type="email"
           placeholder="Insira seu e-mail"
           data-testid="common_register__input-email"
+          onChange={ (event) => setEmail(event.target.value) }
         />
         <input
           type="password"
           placeholder="Insira sua senha"
           data-testid="common_register__input-password"
+          onChange={ (event) => setPassword(event.target.value) }
         />
         <button
           type="button"
           data-testid="common_register__button-register"
+          disabled={ register }
         >
           Cadastrar
         </button>
