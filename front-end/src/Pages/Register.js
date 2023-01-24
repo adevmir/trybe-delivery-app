9caa15f85c5680306918a3cb37538a126ab38c9a
adevmir <ademirvalonqar@gmail.com>
2023-01-24 16:05:19 -0300

    Requisito 06 - Implementa pagina '/register' e funcao do botao da pagina de login que redireciona para tal

---
+++ front-end/src/Pages/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+function Register() {
+  return (
+    <div>
+      <Redirect to="/register" />
+    </div>
+  );
+}
+
+export default Register;
