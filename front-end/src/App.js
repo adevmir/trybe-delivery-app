f9ac633d8724f8ca8f7aa7ed0f2dd8a786b9b564
biancabera <biancabera@gmail.com>
2023-01-25 07:55:35 -0300

    Requisito 11 ainda nao passando

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
+import Products from './Pages/Products';
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={ Login } />
       <Route path="/login" component={ Login } />
+      <Route path="/customer/products" component={ Products } />
     </Switch>
   );
 }
---
+++ front-end/src/Pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../Components/NavBar';
+
+function Products() {
+  return (
+    <div>
+      <NavBar />
+      <p>Products</p>
+    </div>
+  );
+}
+
+export default Products;
