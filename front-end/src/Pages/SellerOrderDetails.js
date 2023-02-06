3f286c83bfa70b06a46e565bdcdb66767581e6fe
adevmir <ademirvalonqar@gmail.com>
2023-02-06 18:17:14 -0300

    css detalhase do pedido do vendedor

--- front-end/src/Pages/SellerOrderDetails.js
+++ front-end/src/Pages/SellerOrderDetails.js
@@ -4,6 +4,7 @@ import OrderDetailsTable from '../Components/OrderDetailsTable';
 import { fixedToTwoDecimalDigits } from '../utils';
 import useSellerSales from '../hooks/useSellerSales';
 import useUserRole from '../hooks/useUserRole';
+import './OrderDetails.css';
 
 export default function OrderDetails() {
   const { saleById, setSalesStatus, pending,
@@ -14,33 +15,33 @@ export default function OrderDetails() {
     <>
       <NavBar />
       <main>
-        <div>
-          {saleById !== null && (
-            <>
-              <OrderDetailsHeader
-                status={ saleStatus }
-                id={ saleById?.id }
-                sellDate={ saleById?.saleDate }
-                seller={ saleById?.seller.name }
-                isSeller={ isSeller }
-                orderRole={ orderRole }
-                setSalesStatus={ setSalesStatus }
-                pending={ pending }
-                deliveryOrder={ deliveryOrder }
-              />
-              <OrderDetailsTable
-                orders={ saleById?.products }
-                orderRole={ orderRole }
-              />
-              <div
-                data-testid={ `${orderRole}_order_details__element-order-total-price` }
-              >
-                Total: R$
-                {fixedToTwoDecimalDigits(saleById?.totalPrice)}
-              </div>
-            </>
-          )}
-        </div>
+        <p className="details-title">Detalhe do Pedido</p>
+        {saleById !== null && (
+          <div className="card-details">
+            <OrderDetailsHeader
+              status={ saleStatus }
+              id={ saleById?.id }
+              sellDate={ saleById?.saleDate }
+              seller={ saleById?.seller.name }
+              isSeller={ isSeller }
+              orderRole={ orderRole }
+              setSalesStatus={ setSalesStatus }
+              pending={ pending }
+              deliveryOrder={ deliveryOrder }
+            />
+            <OrderDetailsTable
+              orders={ saleById?.products }
+              orderRole={ orderRole }
+            />
+            <div
+              data-testid={ `${orderRole}_order_details__element-order-total-price` }
+              className="details-total"
+            >
+              Total: R$
+              {fixedToTwoDecimalDigits(saleById?.totalPrice)}
+            </div>
+          </div>
+        )}
       </main>
     </>
   );
